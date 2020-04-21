43961450080f0871def831af8849a07f39525611
sesegarb <seseg.arbalzhinova@gmail.com>
2020-04-21 21:30:55 +0000

    stage 5

--- mobdev-ca2/src/app/pages/quote-details/quote-details.page.ts
+++ mobdev-ca2/src/app/pages/quote-details/quote-details.page.ts
@@ -1,15 +1,19 @@
+import { ApiService } from './../../services/api.service';
 import { Component, OnInit } from '@angular/core';
-
+import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-quote-details',
   templateUrl: './quote-details.page.html',
   styleUrls: ['./quote-details.page.scss'],
 })
 export class QuoteDetailsPage implements OnInit {
-
-  constructor() { }
-
+  quote: any;
+  quoteId = null;
+  constructor(private activatedRoute: ActivatedRoute, private api: ApiService) {}
   ngOnInit() {
-  }
-
+    this.quoteId = this.activatedRoute.snapshot.paramMap.get('id');
+    this.api.getQuote(this.quoteId).subscribe(res => {
+      this.quote = res[0];
+    })
+    }
 }
